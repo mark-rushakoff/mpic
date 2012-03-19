8bbbcf9423d8739b3c6d9559fe9293b7c12db84a
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-03-18 23:37:45 -0700

    Initial clean commit for gh-pages

--- js/hiding_buttoner.js
+++
@@ -1,9 +0,0 @@
-// Bad implementation: only hides the button
-function HidingButtoner($container) {
-    var counter = 0;
-    return function() {
-        var $button = $("<button/>").text("HidingButtoner " + (++counter));
-        $button.click(function(){ $button.hide(); });
-        $container.append($button);
-    }
-};
--- js/mpic.js
+++
@@ -1,8 +0,0 @@
-function initializeButton(buttonFuncName, buttonId) {
-    var $buttonSection = $("#" + buttonId);
-    var buttonClickFunc = window[buttonFuncName]($buttonSection.find(".output"));
-
-    var $button = $("<button/>").addClass("adder").text(buttonFuncName);
-    $button.click(buttonClickFunc);
-    $buttonSection.find(".input").append($button);
-}
